3f83ab5cfc03ac5a458f23aa4d1b0291a2d02da0
Justin Gorham <jgorham@centeredgesoftware.com>
2020-09-10 18:13:09 -0400

    Squashed commit of the following:
    commit acd0e861400b6d473be2b4d324758011fbc66b63
    Author: Justin Gorham <jgorham@centeredgesoftware.com>
    Date:   Thu Sep 10 18:12:46 2020 -0400
    
        templating complete
    
    commit 692b6880630951eb451233451a78a4fb4563a0c0
    Author: Justin Gorham <jgorham@centeredgesoftware.com>
    Date:   Thu Sep 10 17:48:29 2020 -0400
    
        ready for templating

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
+import createMatcher from 'feather-route-matcher';
+import Handlebars from 'handlebars';
 
 interface RuntimeRequestBody {
     path: string;
     body: any;
+    status?: number;
 }
 
 const isRuntimeRequestBody = (obj: any): obj is RuntimeRequestBody => {
-    return typeof obj.path === 'string' && obj.hasOwnProperty('body')
+    return typeof obj.path === 'string'
+        && obj.hasOwnProperty('body')
+        && (obj.status === undefined || !isNaN(Number(obj.status)))
 }
 
 interface RuntimeRequestCollection {
-    [path: string]: any
+    [path: string]: RuntimeRequestBody
 }
-
 
 export const appFactory = (runtimeCollection?: RuntimeRequestCollection) => {
     const app = express();
@@ -32,7 +36,7 @@ export const appFactory = (runtimeCollection?: RuntimeRequestCollection) => {
             const fixedPath = `/${body.path}`.replace(/\/\//g, '/');
             runtimeRequestCollection = {
                 ...runtimeRequestCollection,
-                [fixedPath]: body.body
+                [fixedPath]: body
             }
             return res.status(202).send();
         }
@@ -40,16 +44,47 @@ export const appFactory = (runtimeCollection?: RuntimeRequestCollection) => {
     })
 
     app.get('/*', (req, res) => {
+
+        const matcher = createMatcher(runtimeRequestCollection);
+
         const query = Object.keys(req.query).reduce((acc, key) => ({
             ...acc,
             [key]: Array.isArray(req.query[key]) ? req.query[key] : [req.query[key]]
         }), {} as any);
 
-        if (runtimeRequestCollection[req.path]) {
-            return res.send(runtimeRequestCollection[req.path]);
+        const routeMatch = matcher(req.path);
+
+        if (routeMatch) {
+            const reqInfo: RuntimeRequestBody = routeMatch.value;
+            const params = routeMatch.params;
+
+            const tokenParams = {
+                ...query,
+                ...params
+            }
+
+            if (reqInfo.status) {
+                res.status(reqInfo.status);
+            }
+
+            const { body } = reqInfo;
+
+            if (typeof body !== 'string') {
+                return res.send(body);
+            }
+
+            const template = Handlebars.compile(body);
+            const result = template(tokenParams)
+
+            try {
+                const jsonResult = JSON.parse(result);
+                return res.send(jsonResult);
+            } catch (_) {
+                return res.send(result)
+            }
         }
 
-        return res.status(404).send(req.path);
+        return res.status(404).send();
     });
 
     return app;
